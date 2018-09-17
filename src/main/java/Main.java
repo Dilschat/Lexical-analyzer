7c0f5d5f7c7b95dc662e23d656ddb0ac8e881625
Dilschat <dilschat@yandex.ru>
2018-09-17 10:14:46 +0300

    Token implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args){
+        Character a = ';';
+        String s;
+        String pattern = "[a-zA-Z_][a-zA-Z0-9_]*";
+        s = "1q_asd";
+        System.out.println(s.matches(pattern));
+    }
+
 }
---
+++ src/main/java/Token.java
@@ -0,0 +1,8 @@
+public class Token {
+    private String element;
+    private String type;
+    public Token(String element, String type) {
+        this.element = element;
+        this.type = type;
+    }
+}
