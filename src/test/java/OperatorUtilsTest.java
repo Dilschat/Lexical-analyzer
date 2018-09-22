c2f3e712b6b81c42c1147b20aafd53d0741470c2
Dilschat <dilschat@yandex.ru>
2018-09-22 23:51:53 +0500

    operator utils tests implemented

--- src/test/java/OperatorUtilsTest.java
+++ src/test/java/OperatorUtilsTest.java
@@ -6,17 +6,30 @@ class OperatorUtilsTest {
 
     @Test
     void processOperator() {
+        assertEquals(OperatorUtils.processOperator("+"),new Token("+",Token.OPERATOR));
+        assertEquals(OperatorUtils.processOperator("+="),new Token("+=",Token.OPERATOR));
     }
 
     @Test
     void isBeginningOperator() {
+        assertTrue(OperatorUtils.isBeginningOperator("+"));
+        assertTrue(OperatorUtils.isBeginningOperator("+="));
+        assertTrue(OperatorUtils.isBeginningOperator("=="));
+        assertFalse(OperatorUtils.isBeginningOperator("sfsaf"));
+        assertFalse(OperatorUtils.isBeginningOperator("±"));
+        assertFalse(OperatorUtils.isBeginningOperator("#"));
     }
 
     @Test
     void isSyntaxNoiseOperator() {
+        assertTrue(OperatorUtils.isSyntaxNoiseOperator(":+"));
+        assertTrue(OperatorUtils.isSyntaxNoiseOperator("::"));
+        assertTrue(OperatorUtils.isSyntaxNoiseOperator(":::"));
+        assertFalse(OperatorUtils.isSyntaxNoiseOperator("+++"));
     }
 
     @Test
     void processSyntaxNoiseOperator() {
+        assertEquals(OperatorUtils.processSyntaxNoiseOperator(":::"),new Token("::",Token.OPERATOR_NOISE));
     }
 }
