ea510246a5bf1a09234bf1ee59ea45423308b6da
Dilschat <dilschat@yandex.ru>
2018-09-23 01:57:01 +0500

    building script added

---
+++ build_and_run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+mvn clean compile assembly:single
+mv ./target/lexical-analyzer-1.0-SNAPSHOT-jar-with-dependencies.jar .
+java -jar lexical-analyzer-1.0-SNAPSHOT-jar-with-dependencies.jar
