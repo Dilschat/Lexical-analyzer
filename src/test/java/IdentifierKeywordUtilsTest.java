30df64c855b95c4f47495343f362dad8217e8329
Dilschat <dilschat@yandex.ru>
2018-09-22 23:35:44 +0500

    identifier/keuword utils tests implmemented

--- src/test/java/IdentifierKeywordUtilsTest.java
+++ src/test/java/IdentifierKeywordUtilsTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,17 +8,36 @@ class IdentifierKeywordUtilsTest {
 
     @Test
     void isIdentifierStart() {
+        assertTrue(IdentifierKeywordUtils.isIdentifierStart("sdfausfa"));
+        assertTrue(IdentifierKeywordUtils.isIdentifierStart("_sdfausfa"));
+        assertTrue(IdentifierKeywordUtils.isIdentifierStart("@sdfausfa"));
+        assertFalse(IdentifierKeywordUtils.isIdentifierStart("123sdfausfa"));
+        assertTrue(IdentifierKeywordUtils.isIdentifierStart("+123sdfausfa"));
+        assertTrue(IdentifierKeywordUtils.isIdentifierStart("/123sdfausfa"));
     }
 
     @Test
     void processIdentifier() {
+        assertEquals(IdentifierKeywordUtils.processIdentifier("qw"),new Token("qw",Token.IDENTIFIER));
+        assertEquals(IdentifierKeywordUtils.processIdentifier("for1"),new Token("qw",Token.IDENTIFIER));
+        assertEquals(IdentifierKeywordUtils.processIdentifier("iff"),new Token("qw",Token.IDENTIFIER));
+        assertEquals(IdentifierKeywordUtils.processIdentifier("if"),new Token("if",Token.KEYWORD));
+        assertEquals(IdentifierKeywordUtils.processIdentifier("for"),new Token("if",Token.KEYWORD));
+        assertEquals(IdentifierKeywordUtils.processIdentifier("catch"),new Token("if",Token.KEYWORD));
+        assertEquals(IdentifierKeywordUtils.processIdentifier("else"),new Token("if",Token.KEYWORD));
     }
 
     @Test
     void isStartOfStrangeIdentifier() {
+        assertTrue(IdentifierKeywordUtils.isStartOfStrangeIdentifier('`'));
+        assertTrue(IdentifierKeywordUtils.isStartOfStrangeIdentifier('d'));
     }
 
     @Test
     void processStrangeIdentifier() {
+        assertEquals(IdentifierKeywordUtils.processIdentifier("`qw`"),new Token("`qw`",Token.IDENTIFIER));
+        Executable closureContainingCodeToTest = () -> { IdentifierKeywordUtils.processStrangeIdentifier("`");};
+        assertThrows(Exception.class,closureContainingCodeToTest,"Incorrect strange identifier: `");
+
     }
 }
